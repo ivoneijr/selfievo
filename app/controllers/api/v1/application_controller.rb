baaab0ab2795e3bb73bcb66df004fa116a7981fd
Ivonei Junior <ivonei.marques@zeta.com.br>
2014-10-25 11:43:52 -0200

    change appcontroller

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,14 +1,14 @@
 class API::V1::ApplicationController <  ActionController::Base
 
-	 include ActionController::StrongParameters
-	 include ActionController::Rendering
-	 include ActionController::Renderers::All
-	 include ActionController::Serialization
-	 include ActionController::Redirecting
-	 include Rails.application.routes.url_helpers
+	include ActionController::StrongParameters
+	include ActionController::Rendering
+	include ActionController::Renderers::All
+	include ActionController::Serialization
+	include ActionController::Redirecting
+	include Rails.application.routes.url_helpers
 
 
-  protect_from_forgery with: :null_session
+	protect_from_forgery with: :null_session
 
 
  end
